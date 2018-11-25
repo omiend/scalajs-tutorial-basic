7a9ddda15e43a5f9fa8857693130eb7a17cd0206
omiend <omiend@gmail.com>
2018-11-25 22:55:00 +0900

    Step 5: Using jQuery

--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -1,30 +1,40 @@
 package tutorial.webapp
 
 // 「org.scalajs.dom」はJavaScript DOMのルートで、JavaScriptのグローバルスコープ（window）に対応
-import org.scalajs.dom
+// import org.scalajs.dom
 // 便宜上、ドキュメント（JavaScriptのドキュメントに対応）を追加インポート
-import dom.document
+// import dom.document
+// import scala.scalajs.js.annotation.JSExportTopLevel
 
-import scala.scalajs.js.annotation.JSExportTopLevel
+import org.querki.jquery._
 
 object TutorialApp {
 
   def main(args: Array[String]): Unit = {
-    appendPar(document.body, "Hello World")
+    // appendPar(document.body, "Hello World")
+    $(() => setupUI())
   }
 
   // DOM操作
-  def appendPar(targetNode: dom.Node, text: String): Unit = {
-    val parNode = document.createElement("p")
-    val textNode = document.createTextNode(text)
-    parNode.appendChild(textNode)
-    targetNode.appendChild(parNode)
-  }
+  // def appendPar(targetNode: dom.Node, text: String): Unit = {
+  //   val parNode = document.createElement("p")
+  //   val textNode = document.createTextNode(text)
+  //   parNode.appendChild(textNode)
+  //   targetNode.appendChild(parNode)
+  // }
 
   // @JSExportTopLevel = Scala.jsコンパイラに、そのメソッドをJavaScriptから最上位関数として呼び出し可能にするよう指示する
   // このアノテーションもインポートする必要あり
-  @JSExportTopLevel("addClickedMessage")
+  // @JSExportTopLevel("addClickedMessage")
   def addClickedMessage(): Unit = {
-    appendPar(document.body, "You clicked the button!")
+    // appendPar(document.body, "You clicked the button!")
+    $("body").append("<p>You clicked the button!</p>")
   }
+
+  def setupUI(): Unit = {
+    $("body").append("<p>Hello World</p>")
+    $("#click-me-button").click(() => addClickedMessage())
+    println("TEST")
+  }
+
 }

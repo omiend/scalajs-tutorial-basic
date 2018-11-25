21543671c44ed0be0ef91c28c479c316b3fd78ee
omiend <omiend@gmail.com>
2018-11-25 22:06:53 +0900

    Step 4: Reacting on User Input

--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -5,15 +5,26 @@ import org.scalajs.dom
 // 便宜上、ドキュメント（JavaScriptのドキュメントに対応）を追加インポート
 import dom.document
 
+import scala.scalajs.js.annotation.JSExportTopLevel
+
 object TutorialApp {
+
   def main(args: Array[String]): Unit = {
     appendPar(document.body, "Hello World")
   }
 
+  // DOM操作
   def appendPar(targetNode: dom.Node, text: String): Unit = {
     val parNode = document.createElement("p")
     val textNode = document.createTextNode(text)
     parNode.appendChild(textNode)
     targetNode.appendChild(parNode)
+  }
+
+  // @JSExportTopLevel = Scala.jsコンパイラに、そのメソッドをJavaScriptから最上位関数として呼び出し可能にするよう指示する
+  // このアノテーションもインポートする必要あり
+  @JSExportTopLevel("addClickedMessage")
+  def addClickedMessage(): Unit = {
+    appendPar(document.body, "You clicked the button!")
   }
 }
